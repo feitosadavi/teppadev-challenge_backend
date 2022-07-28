df7c083c09bcfc901c0398f2db82025173e0cea7
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-28 18:00:53 -0300

    feat(CreateAccount)!: ensure it calls hasher with correct input

--- src/application/usecases/create-account.ts
+++ src/application/usecases/create-account.ts
@@ -2,17 +2,20 @@ import {
   ICreateAccount,
   ILoadAccountByEmailRepository,
   ICreateAccountRepository,
+  Hasher
 } from '@/application/protocols'
 
 export class CreateAccount implements ICreateAccount {
   constructor(
     private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
+    private readonly hasher: Hasher,
     private readonly createAccountRepository: ICreateAccountRepository,
   ) { }
 
   async execute ({ email, password }: ICreateAccount.Input): Promise<ICreateAccount.Output | null> {
     const account = await this.loadAccountByEmailRepository.loadByEmail({ email })
     if (account) return null
+    this.hasher.hash(password);
     await this.createAccountRepository.create({ email, password })
   }
 }
