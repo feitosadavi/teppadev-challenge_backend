04129d5bb55d96717fc9a9e162fc41e91e9e7a4f
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-28 18:56:19 -0300

    test(SignupController)!: ensure it returns 400 if createAccount returns null

--- tests/application/controllers/signup.controller.spec.ts
+++ tests/application/controllers/signup.controller.spec.ts
@@ -3,6 +3,7 @@ import { MockProxy, mock } from 'jest-mock-extended'
 import {
   ISignup,
   ICreateAccount,
+  ICreateRestaurant
 } from '@/application/protocols'
 import { SignupController } from '@/application/controllers'
 
@@ -19,14 +20,17 @@ const makeFakeRequest = (): ISignup.Input => ({
 describe('SignupController', () => {
   let sut: SignupController
   let fakeCreateAccount: MockProxy<ICreateAccount>
+  let fakeCreateRestaurant: MockProxy<ICreateRestaurant>
   let fakeRequest: ISignup.Input
 
   beforeAll(() => {
     fakeCreateAccount = mock()
+    fakeCreateAccount.execute.mockReturnValue(Promise.resolve({ accessToken: 'any_access_token' }))
+    fakeCreateRestaurant = mock()
     fakeRequest = makeFakeRequest()
   })
   beforeEach(() => {
-    sut = new SignupController(fakeCreateAccount)
+    sut = new SignupController(fakeCreateAccount, fakeCreateRestaurant)
   })
 
   it('should call createAccount with correct input', async () => {
@@ -34,5 +38,15 @@ describe('SignupController', () => {
 
     expect(fakeCreateAccount.execute)
       .toHaveBeenCalledWith(fakeRequest.accountInput)
+  })
+
+  it('should return 400 if createAccount returns null', async () => {
+    fakeCreateAccount.execute.mockReturnValueOnce(null)
+    const response = await sut.handle(fakeRequest)
+    expect(response)
+      .toEqual({
+        statusCode: 400,
+        body: 'O email inserido já está em uso'
+      })
   })
 })
