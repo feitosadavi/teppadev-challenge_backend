3adfccdd8025b4f21e64166bfe6edef552185d69
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-28 17:50:44 -0300

    test(CreateAccount)!: ensure it returns loadAccountByEmailRepository returns an account

--- tests/application/usecases/create-account.spec.ts
+++ tests/application/usecases/create-account.spec.ts
@@ -6,11 +6,17 @@ import {
   ICreateAccountRepository,
   ILoadAccountByEmailRepository
 } from '@/application/protocols'
+import { Account } from '@/domain/entities'
 
 const makeFakeCreateAccountInput = (): ICreateAccount.Input => ({
   email: 'any@email.com',
   password: 'any_password'
 })
+
+const makeFakeAccount = (): Account => {
+  const params = makeFakeCreateAccountInput()
+  return { id: 'any_id', ...params }
+}
 
 describe('CreateAccount', () => {
   let sut: CreateAccount
@@ -37,10 +43,16 @@ describe('CreateAccount', () => {
       .toHaveBeenCalledWith({ email })
   })
 
-  it('should call createAccountRepository with correct input', async () => {
-    await sut.execute(fakeCreateAccountInput)
-
-    expect(fakeCreateAccountRepository.create)
-      .toHaveBeenCalledWith(fakeCreateAccountInput)
+  it('should call loadAccountByEmailRepository with correct input', async () => {
+    fakeLoadAccountByEmailRepository.loadByEmail.mockResolvedValueOnce(makeFakeAccount())
+    const account = await sut.execute(fakeCreateAccountInput)
+    expect(account).toBeNull()
   })
+
+  // it('should call createAccountRepository with correct input', async () => {
+  //   await sut.execute(fakeCreateAccountInput)
+
+  //   expect(fakeCreateAccountRepository.create)
+  //     .toHaveBeenCalledWith(fakeCreateAccountInput)
+  // })
 })
