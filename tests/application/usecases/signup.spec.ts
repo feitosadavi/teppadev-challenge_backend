3560c4db34c73f2cc22f798479f93be0abd1ca71
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-28 15:27:19 -0300

    test(Signup): ensure it calls createRestaurantRepository with correct input

--- tests/application/usecases/signup.spec.ts
+++ tests/application/usecases/signup.spec.ts
@@ -1,7 +1,11 @@
 import { MockProxy, mock } from 'jest-mock-extended'
 
 import { Signup } from '@/application/usecases'
-import { ISignup, ICreateAccountRepository } from '@/application/protocols'
+import {
+  ISignup,
+  ICreateAccountRepository,
+  ICreateRestaurantRepository
+} from '@/application/protocols'
 
 const makeFakeSignupInput = (): ISignup.Input => ({
   accountInput: {
@@ -16,22 +20,29 @@ const makeFakeSignupInput = (): ISignup.Input => ({
 describe('Signup', () => {
   let sut: Signup
   let fakeCreateAccountRepository: MockProxy<ICreateAccountRepository>
+  let fakeCreateRestaurantRepository: MockProxy<ICreateRestaurantRepository>
   let fakeSignupInput: ISignup.Input
 
   beforeAll(() => {
     fakeCreateAccountRepository = mock()
+    fakeCreateRestaurantRepository = mock()
     fakeSignupInput = makeFakeSignupInput()
   })
   beforeEach(() => {
-    sut = new Signup(fakeCreateAccountRepository)
+    sut = new Signup(fakeCreateAccountRepository, fakeCreateRestaurantRepository)
   })
 
   it('should call createAccountRepository with correct input', async () => {
     await sut.execute(fakeSignupInput)
 
-    expect(fakeCreateAccountRepository.create).toHaveBeenCalledWith({
-      email: 'any@email.com',
-      password: 'any_password'
-    })
+    expect(fakeCreateAccountRepository.create)
+      .toHaveBeenCalledWith(fakeSignupInput.accountInput)
+  })
+
+  it('should call createAccountRepository with correct input', async () => {
+    await sut.execute(fakeSignupInput)
+
+    expect(fakeCreateRestaurantRepository.create)
+      .toHaveBeenCalledWith(fakeSignupInput.restaurantInput)
   })
 })
