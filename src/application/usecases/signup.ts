9d488bafed9bbc23893455c3e034d2125e4f3b38
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-28 15:27:08 -0300

    feat(Signup): ensure it calls createRestaurantRepository with correct input

--- src/application/usecases/signup.ts
+++ src/application/usecases/signup.ts
@@ -1,11 +1,21 @@
-import { ISignup, ICreateAccountRepository } from '@/application/protocols'
+import {
+  ISignup,
+  ICreateAccountRepository,
+  ICreateRestaurantRepository
+} from '@/application/protocols'
 
 export class Signup implements ISignup {
-  constructor(private readonly createAccountRepository: ICreateAccountRepository) { }
+  constructor(
+    private readonly createAccountRepository: ICreateAccountRepository,
+    private readonly createRestaurantRepository: ICreateRestaurantRepository
+  ) { }
 
   async execute (input: ISignup.Input): Promise<ISignup.Output> {
-    const { accountInput } = input
-    await this.createAccountRepository.create(accountInput)
+    const { accountInput, restaurantInput } = input
+    Promise.all([
+      this.createAccountRepository.create(accountInput),
+      this.createRestaurantRepository.create(restaurantInput)
+    ])
     return
   }
 }
