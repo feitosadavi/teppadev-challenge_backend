113aa24e4e62723fe8474d675ea4aaf1e94c01bd
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-28 21:47:27 -0300

    refactor(CreateAccount)!: remove loadAccountByIdRepository from it

--- src/application/usecases/create-account.ts
+++ src/application/usecases/create-account.ts
@@ -1,6 +1,5 @@
 import {
   ICreateAccount,
-  ILoadAccountByEmailRepository,
   ICreateAccountRepository,
   Hasher,
   IAuthenticator
@@ -8,20 +7,15 @@ import {
 
 export class CreateAccount implements ICreateAccount {
   constructor(
-    private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
     private readonly hasher: Hasher,
     private readonly createAccountRepository: ICreateAccountRepository,
     private readonly authentication: IAuthenticator,
   ) { }
 
   async execute ({ email, password }: ICreateAccount.Input): Promise<ICreateAccount.Output | null> {
-    const account = await this.loadAccountByEmailRepository.loadByEmail({ email })
-    if (account) return null
-    console.log({ account })
     const hashedPassword = await this.hasher.hash(password);
     await this.createAccountRepository.create({ email, password: hashedPassword })
     const accessToken = await this.authentication.authenticate({ email, password })
-    console.log({ accessToken })
     if (!accessToken) return null
     return { accessToken }
   }
--- tests/application/usecases/create-account.spec.ts
+++ tests/application/usecases/create-account.spec.ts
@@ -4,7 +4,6 @@ import { CreateAccount } from '@/application/usecases'
 import {
   ICreateAccount,
   ICreateAccountRepository,
-  ILoadAccountByEmailRepository,
   Hasher,
   IAuthenticator
 } from '@/application/protocols'
@@ -23,7 +22,6 @@ const makeFakeAccount = (): Account => {
 describe('CreateAccount', () => {
   let sut: CreateAccount
 
-  let fakeLoadAccountByEmailRepository: MockProxy<ILoadAccountByEmailRepository>
   let fakeHasher: MockProxy<Hasher>
   let fakeCreateAccountRepository: MockProxy<ICreateAccountRepository>
   let fakeAuthenticator: MockProxy<IAuthenticator>
@@ -32,9 +30,6 @@ describe('CreateAccount', () => {
   let fakeAccount: Account
 
   beforeAll(() => {
-    fakeLoadAccountByEmailRepository = mock()
-    fakeLoadAccountByEmailRepository.loadByEmail.mockResolvedValue(null)
-
     fakeCreateAccountRepository = mock()
 
     fakeHasher = mock()
@@ -48,24 +43,10 @@ describe('CreateAccount', () => {
   })
   beforeEach(() => {
     sut = new CreateAccount(
-      fakeLoadAccountByEmailRepository,
       fakeHasher,
       fakeCreateAccountRepository,
       fakeAuthenticator
     )
-  })
-
-  it('should call loadAccountByEmailRepository with correct input', async () => {
-    await sut.execute(fakeCreateAccountInput)
-    const { email } = fakeCreateAccountInput
-    expect(fakeLoadAccountByEmailRepository.loadByEmail)
-      .toHaveBeenCalledWith({ email })
-  })
-
-  it('should return null if loadAccountByEmailRepository returns an account', async () => {
-    fakeLoadAccountByEmailRepository.loadByEmail.mockResolvedValueOnce(fakeAccount)
-    const account = await sut.execute(fakeCreateAccountInput)
-    expect(account).toBeNull()
   })
 
   it('should call hasher with correct input', async () => {
