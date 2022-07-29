420c19df2afa2fc6c8aeb9b38f68b0e82e8b104f
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-28 21:35:34 -0300

    test(LoadAccountByEmail)!: ensure it calls loadAccountByEmailRepository with correct input

---
+++ tests/application/usecases/load-account-by-email.spec.ts
@@ -0,0 +1,45 @@
+import { MockProxy, mock } from 'jest-mock-extended'
+
+import { LoadAccountByEmail } from '@/application/usecases'
+import {
+  ILoadAccountByEmail,
+  ILoadAccountByEmailRepository
+} from '@/application/protocols'
+import { Account } from '@/domain/entities'
+
+const makeFakeLoadAccountInput = (): ILoadAccountByEmail.Input => ({
+  email: 'any@email.com',
+})
+
+const makeFakeAccount = (): Account => {
+  const params = makeFakeLoadAccountInput()
+  return { id: 'any_id', ...params, password: 'any_password' }
+}
+
+describe('CreateAccount', () => {
+  let sut: LoadAccountByEmail
+
+  let fakeLoadAccountByEmailRepository: MockProxy<ILoadAccountByEmailRepository>
+
+  let fakeAccount: Account
+  let fakeLoadAccountInput: ILoadAccountByEmail.Input
+
+  beforeAll(() => {
+    fakeLoadAccountByEmailRepository = mock()
+    fakeLoadAccountByEmailRepository.loadByEmail.mockResolvedValue(null)
+    fakeLoadAccountInput = makeFakeLoadAccountInput()
+    fakeAccount = makeFakeAccount()
+  })
+  beforeEach(() => {
+    sut = new LoadAccountByEmail(
+      fakeLoadAccountByEmailRepository
+    )
+  })
+
+  it('should call loadAccountByEmailRepository with correct input', async () => {
+    await sut.execute(fakeLoadAccountInput)
+    const { email } = fakeLoadAccountInput
+    expect(fakeLoadAccountByEmailRepository.loadByEmail)
+      .toHaveBeenCalledWith({ email })
+  })
+})
