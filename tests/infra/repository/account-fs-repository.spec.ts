23cf1aabb0b60c2c94caf35601287cdd5e67cbcc
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-30 16:55:13 -0300

    test(AccountFsRepository): ensure loadByEmail loads a account by its email

--- tests/infra/repository/account-fs-repository.spec.ts
+++ tests/infra/repository/account-fs-repository.spec.ts
@@ -12,36 +12,44 @@ describe('JWTAdapter', () => {
   let db: FirebaseFirestore.Firestore
 
   beforeAll(() => {
+    initializeApp({
+      credential: cert(FS_KEY as any)
+    });
+
+    db = getFirestore();
   })
 
   beforeEach(() => {
     sut = new AccountFsRepository()
   })
 
+  afterEach(async () => {
+    // Clear database
+    const accountsCollection = db.collection('accounts')
+    const querySnapshot = await accountsCollection.get()
+    querySnapshot.forEach(doc => doc.ref.delete())
+  })
+
   describe('CreateAccountRepository', () => {
-    beforeAll(() => {
-
-      initializeApp({
-        credential: cert(FS_KEY as any)
-      });
-
-      db = getFirestore();
-
-    })
-
-    afterEach(async () => {
-      // Clear database
-      const accountsCollection = db.collection('accounts')
-      const querySnapshot = await accountsCollection.get()
-      querySnapshot.forEach(doc => doc.ref.delete())
-    })
-
     it('should add a account and return its id', async () => {
       const id = await sut.create({
         email: 'any@email.com',
         password: 'any_password'
       })
       expect(id).toBeTruthy()
+    })
+  })
+
+  describe('LoadAccountRepository', () => {
+    it('should load a account by its email', async () => {
+      let email = 'any@email.com'
+      let password = 'hashed_password'
+
+      await db.collection('accounts').add({ email, password })
+
+      const account = await sut.loadByEmail({ email: 'any@email.com' })
+      expect(account.id).toBeTruthy()
+      expect(account.email).toBe('any@email.com')
     })
   })
 })
