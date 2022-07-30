3bd514960e99ec26a4e8b652884a8a30cdac1f76
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-30 11:45:39 -0300

    test(JWTAdapter): ensure it returns a token on success

--- tests/infra/cryptography/jwt-adapter.spec.ts
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -6,22 +6,39 @@ jest.mock('jsonwebtoken')
 describe('JWTAdapter', () => {
   let sut: JWTAdapter
   let fakeJwt = jwt as jest.Mocked<typeof jwt>
-  const SECRET = 'secret'
+
+  let secret: string
+
   beforeAll(() => {
+    secret = 'secret'
     fakeJwt = jwt as jest.Mocked<typeof jwt>
   })
 
   beforeEach(() => {
-    sut = new JWTAdapter(SECRET)
+    sut = new JWTAdapter(secret)
   })
 
   describe('generate', () => {
-    const makeFakeGenerateInput = () => ({ key: 'any_id' })
+    let token: string
+    let key: string
+
+    beforeAll(() => {
+      token = 'any_token'
+      key = 'any_key'
+
+      fakeJwt.sign.mockImplementation(() => token)
+    })
 
     it('should call sign with correct params', async () => {
-      const fakeGenerateInput = makeFakeGenerateInput()
-      await sut.generate(fakeGenerateInput)
-      expect(fakeJwt.sign).toHaveBeenCalledWith(fakeGenerateInput, SECRET)
+      await sut.generate({ key })
+
+      expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret)
+    })
+
+    it('should return a token on success', async () => {
+      const generatedToken = await sut.generate({ key })
+
+      expect(generatedToken).toBe(token)
     })
   })
 
