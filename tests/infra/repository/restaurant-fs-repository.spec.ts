273b7f91046471f9bb5c07ad14ee94e4fef3b5d3
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-30 15:00:51 -0300

    test(RestaurantFsRepository): ensure CreateRestaurantRepository adds a restaurant on success

---
+++ tests/infra/repository/restaurant-fs-repository.spec.ts
@@ -0,0 +1,45 @@
+
+import { initializeApp, cert } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+import { RestaurantFsRepository } from '@/infra/repository'
+import FS_KEY from '@/../fs-key.json'
+
+
+describe('RestaurantFsRepository', () => {
+  let sut: RestaurantFsRepository
+  let db: FirebaseFirestore.Firestore
+
+  beforeAll(() => {
+  })
+
+  beforeEach(() => {
+    sut = new RestaurantFsRepository()
+  })
+
+  describe('CreateRestaurantRepository', () => {
+    beforeAll(() => {
+
+      initializeApp({
+        credential: cert(FS_KEY as any)
+      });
+
+      db = getFirestore();
+
+    })
+
+    afterEach(async () => {
+      // Clear database
+      const accountsCollection = db.collection('accounts')
+      const querySnapshot = await accountsCollection.get()
+      querySnapshot.forEach(doc => doc.ref.delete())
+    })
+
+    it('should add a restaurant', async () => {
+      await sut.create({
+        name: 'any_name'
+      })
+      const restaurant = await db.collection('restaurants').where('name', '==', 'any_name').get()
+      expect(restaurant.docs[0].data()).toEqual({ name: 'any_name' })
+    })
+  })
+})
