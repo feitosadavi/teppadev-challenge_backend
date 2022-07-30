5e0f1f6c78daa7449350f9cbabb927951bb6020b
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-30 14:48:03 -0300

    refactor(CreateAccount): remove unecessary condition

--- src/application/usecases/create-account.ts
+++ src/application/usecases/create-account.ts
@@ -12,11 +12,10 @@ export class CreateAccount implements ICreateAccount {
     private readonly tokenGenerator: TokenGenerator,
   ) { }
 
-  async execute ({ email, password }: ICreateAccount.Input): Promise<ICreateAccount.Output | null> {
+  async execute ({ email, password }: ICreateAccount.Input): Promise<ICreateAccount.Output> {
     const hashedPassword = await this.hasher.hash(password);
     const id = await this.createAccountRepository.create({ email, password: hashedPassword })
     const accessToken = await this.tokenGenerator.generate({ key: id })
-    if (!accessToken) return null
     return { accessToken }
   }
 }
