07fba0062892dc54afe5fa873708aae37ebdc109
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-30 16:24:25 -0300

    feat(BcryptAdapter): ensure hash returns a hashedValue on success

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -5,7 +5,7 @@ export class BcryptAdapter implements Hasher {
   constructor(private readonly salt: number) { }
 
   async hash (input: string): Promise<string> {
-    const hash = await bcrypt.hash(input, this.salt)
-    return
+    const hashedValue = await bcrypt.hash(input, this.salt)
+    return hashedValue
   }
 }
