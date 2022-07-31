2d171e25bcf9b7888029ea312def4f7404fe1b7a
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-31 17:10:57 -0300

    test(Authenticator): ensure it calls hashComparer with correct input

---
+++ tests/application/usecases/authenticator.spec.ts
@@ -0,0 +1,41 @@
+import { MockProxy, mock } from 'jest-mock-extended'
+
+import { Authenticator } from '@/application/usecases'
+import {
+  IAuthenticator,
+  IHashComparer,
+} from '@/application/protocols'
+
+const makeFakeAuthenticatorInput = (): IAuthenticator.Input => ({
+  password: 'any_password',
+  accountPassword: 'hashed_password',
+  accountId: 'any_id'
+})
+
+describe('Authenticator', () => {
+  let sut: Authenticator
+
+  let fakeHashComparer: MockProxy<IHashComparer>
+
+  let fakeAuthenticatorInput: IAuthenticator.Input
+
+  beforeAll(() => {
+    fakeHashComparer = mock()
+    fakeHashComparer.compare.mockResolvedValue(true)
+
+    fakeAuthenticatorInput = makeFakeAuthenticatorInput()
+  })
+  beforeEach(() => {
+    sut = new Authenticator(
+      fakeHashComparer,
+    )
+  })
+
+  it('should call hashComparer with correct input', async () => {
+    await sut.execute(fakeAuthenticatorInput)
+    const { password, accountPassword } = fakeAuthenticatorInput
+
+    expect(fakeHashComparer.compare)
+      .toHaveBeenCalledWith({ hash: accountPassword, value: password })
+  })
+})
