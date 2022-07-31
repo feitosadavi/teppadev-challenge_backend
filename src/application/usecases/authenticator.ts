c942651fab360abba926f16cdf285ecca6549f05
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-31 17:13:36 -0300

    feat(Authenticator): ensure it returns null if hashComparer returns null

--- src/application/usecases/authenticator.ts
+++ src/application/usecases/authenticator.ts
@@ -12,10 +12,10 @@ export class Authenticator implements IAuthenticator {
 
   async execute ({ password, accountPassword, accountId }: IAuthenticator.Input): Promise<IAuthenticator.Output> {
     const matched = await this.hashComparer.compare({ value: password, hash: accountPassword });
-    return
-    // if (matched) {
-    //   const accessToken = await this.tokenGenerator.generate({ key: accountId })
-    //   return accessToken
-    // }
+    if (matched) {
+      // const accessToken = await this.tokenGenerator.generate({ key: accountId })
+      // return accessToken
+    }
+    return null
   }
 }
