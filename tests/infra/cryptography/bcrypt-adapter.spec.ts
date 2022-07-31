4b3f376ad73ef863a0b6bf9dbb9247f73b876bcf
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-31 18:53:14 -0300

    test(BcryptAdapter)!: implement hashComparer

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,3 +1,4 @@
+import { IHashComparer } from '@/application/protocols'
 import { BcryptAdapter } from '@/infra/cryptography'
 
 import bcrypt from 'bcrypt'
@@ -45,6 +46,45 @@ describe('BcryptAdapter', () => {
       fakeBcrypt.hash.mockImplementationOnce(() => { throw new Error('encryption_error') })
 
       const promise = sut.hash(input)
+
+      await expect(promise).rejects.toThrow(new Error('encryption_error'))
+    })
+  })
+
+  describe('compare', () => {
+    let input: IHashComparer.Input
+
+    beforeAll(() => {
+      fakeBcrypt.compare.mockImplementation(() => true)
+      input = {
+        value: 'any_value',
+        hash: 'any_hash'
+      }
+    })
+
+
+    it('should call hash with correct params', async () => {
+      await sut.compare(input)
+
+      expect(fakeBcrypt.compare).toHaveBeenCalledWith(input.value, input.hash)
+    })
+
+    it('should return a false if input values didnt match', async () => {
+      fakeBcrypt.compare.mockImplementationOnce(() => false)
+      const hashedInput = await sut.compare(input)
+      expect(hashedInput).toBe(false)
+    })
+
+    it('should return a true on success', async () => {
+      const hashedInput = await sut.compare(input)
+
+      expect(hashedInput).toBe(true)
+    })
+
+    it('should rethrow if sign throws', async () => {
+      fakeBcrypt.compare.mockImplementationOnce(() => { throw new Error('encryption_error') })
+
+      const promise = sut.compare(input)
 
       await expect(promise).rejects.toThrow(new Error('encryption_error'))
     })
