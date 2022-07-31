2273d0d2d5509bfc3ff0817727d9919d8f901845
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-31 18:31:40 -0300

    feat(Authenticator)!: ensure it calls updateAccountRepository with correct params

--- src/application/usecases/authenticator.ts
+++ src/application/usecases/authenticator.ts
@@ -1,19 +1,22 @@
 import {
   IAuthenticator,
   IHashComparer,
-  TokenGenerator
+  TokenGenerator,
+  IUpdateAccountRepository
 } from '@/application/protocols'
 
 export class Authenticator implements IAuthenticator {
   constructor(
     private readonly hashComparer: IHashComparer,
     private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccountRepository: IUpdateAccountRepository,
   ) { }
 
   async execute ({ password, accountPassword, accountId }: IAuthenticator.Input): Promise<IAuthenticator.Output> {
     const matched = await this.hashComparer.compare({ value: password, hash: accountPassword });
     if (matched) {
       const accessToken = await this.tokenGenerator.generate({ key: accountId })
+      await this.updateAccountRepository.update({ accountId, data: { accessToken } })
       return accessToken
     }
     return null
