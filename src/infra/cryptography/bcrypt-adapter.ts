c5029539bb55a8e1a6afa979b0882afc550562d8
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-31 18:52:56 -0300

    feat(BcryptAdapter)!: implement hashComparer

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,11 +1,16 @@
 import bcrypt from 'bcrypt'
-import { Hasher } from '@/application/protocols'
+import { Hasher, IHashComparer } from '@/application/protocols'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, IHashComparer {
   constructor(private readonly salt: number) { }
 
   async hash (input: string): Promise<string> {
     const hashedValue = await bcrypt.hash(input, this.salt)
     return hashedValue
+  }
+
+  async compare ({ value, hash }: IHashComparer.Input): Promise<IHashComparer.Output> {
+    const matched = await bcrypt.compare(value, hash)
+    return matched
   }
 }
