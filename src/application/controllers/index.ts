07b57cdc78e80c7d4a8c99170bcefec6a269908a
feitosadavi <davifeitosa.dev@protonmail.com>
2022-08-02 17:47:41 -0300

    refactor: move controllers to a separated folder

--- src/application/controllers/index.ts
+++ src/application/controllers/index.ts
@@ -1,4 +1,2 @@
-export * from './signup.controller'
-export * from './login.controller'
-export * from './update-account.controller'
-export * from './update-restaurant.controller'
+export * from './account'
+export * from './restaurant'
