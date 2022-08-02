d0bcd4ca8ddf7ad98700f69b175a8b7d94cbfc98
feitosadavi <davifeitosa.dev@protonmail.com>
2022-08-01 22:26:20 -0300

    feat(RestaurantFsRepository)!: implement loadRestaurantById

--- src/infra/repository/firestore/restaurant-fs-repositoryt.ts
+++ src/infra/repository/firestore/restaurant-fs-repositoryt.ts
@@ -1,7 +1,8 @@
-import { ICreateRestaurantRepository, IUpdateRestaurantRepository } from '@/application/protocols';
+import { ICreateRestaurantRepository, ILoadRestaurantByIdRepository, IUpdateRestaurantRepository } from '@/application/protocols';
+import { Restaurant } from '@/domain/entities';
 import { getFirestore } from 'firebase-admin/firestore'
 
-export class RestaurantFsRepository implements ICreateRestaurantRepository, IUpdateRestaurantRepository {
+export class RestaurantFsRepository implements ICreateRestaurantRepository, IUpdateRestaurantRepository, ILoadRestaurantByIdRepository {
   private readonly restaurantsCollection: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>
 
   constructor() {
@@ -10,6 +11,14 @@ export class RestaurantFsRepository implements ICreateRestaurantRepository, IUpd
 
   async create (input: ICreateRestaurantRepository.Input): Promise<ICreateRestaurantRepository.Output> {
     await this.restaurantsCollection.add(input)
+  }
+
+  async loadById ({ id }: ILoadRestaurantByIdRepository.Input): Promise<ILoadRestaurantByIdRepository.Output> {
+    const data = (await this.restaurantsCollection.doc(id).get()).data()
+    return data ? {
+      id: id,
+      ...data as Omit<Restaurant, 'id'>
+    } : null
   }
 
   async update ({ restaurantId, data }: IUpdateRestaurantRepository.Input): Promise<IUpdateRestaurantRepository.Output> {
