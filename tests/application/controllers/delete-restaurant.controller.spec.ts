a46fa85a08676b36b2b884d08d0cb38f8f27adcc
feitosadavi <davifeitosa.dev@protonmail.com>
2022-08-02 19:52:24 -0300

    test(DeleteRestaurantController): ensure it returns 403 if logged accountId isnt equal to restaurant accountId

--- tests/application/controllers/delete-restaurant.controller.spec.ts
+++ tests/application/controllers/delete-restaurant.controller.spec.ts
@@ -2,11 +2,19 @@ import { MockProxy, mock } from 'jest-mock-extended'
 
 import { IDeleteRestaurant, ILoadRestaurantById } from '@/application/protocols'
 import { DeleteRestaurantController } from '@/application/controllers'
-import { noContent, serverError } from '@/application/helpers'
+import { noContent, serverError, forbidden } from '@/application/helpers'
+import { RestaurantDoesntBelongsToAccountError } from '@/application/errors'
+import { Restaurant } from '@/domain/entities'
 
 const makeFakeRequest = (): DeleteRestaurantController.Request => ({
   restaurantId: 'any_id',
   accountId: 'any_account_id',
+})
+
+const makeFakeRestaurant = (): Restaurant => ({
+  id: 'any_id',
+  name: 'any_name',
+  accountId: 'any_account_id'
 })
 
 describe('DeleteRestaurantController', () => {
@@ -19,6 +27,7 @@ describe('DeleteRestaurantController', () => {
 
   beforeAll(() => {
     fakeLoadRestaurantById = mock()
+    fakeLoadRestaurantById.execute.mockResolvedValue(makeFakeRestaurant())
     fakeDeleteRestaurant = mock()
 
     fakeRequest = makeFakeRequest()
@@ -33,11 +42,20 @@ describe('DeleteRestaurantController', () => {
     expect(fakeLoadRestaurantById.execute).toHaveBeenCalledWith({ id })
   })
 
+  it('should return 403 if logged accountId isnt equal to restaurant accountId', async () => {
+    fakeLoadRestaurantById.execute
+      .mockReturnValueOnce(Promise.resolve({ ...makeFakeRestaurant(), accountId: 'other_account_id' }))
+    const response = await sut.handle(fakeRequest)
+
+    expect(response).toEqual(forbidden(new RestaurantDoesntBelongsToAccountError()))
+  })
+
   it('should call deleteRestaurant with correct input', async () => {
     await sut.handle(fakeRequest)
     const { restaurantId } = fakeRequest
     expect(fakeDeleteRestaurant.execute).toHaveBeenCalledWith({ restaurantId })
   })
+
 
   it('should return 200 on success', async () => {
     const response = await sut.handle(fakeRequest)
