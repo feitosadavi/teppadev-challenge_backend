16eedfa34f75f44e98763487fd1a1c9627121911
feitosadavi <davifeitosa.dev@protonmail.com>
2022-08-02 17:44:41 -0300

    refactor!: add protocols to a single file

---
+++ src/application/protocols/usecases/account-usecase.protocol.ts
@@ -0,0 +1,44 @@
+import { Account } from '@/domain/entities'
+
+export interface ICreateAccount {
+  execute (input: ICreateAccount.Input): Promise<ICreateAccount.Output>
+}
+
+export namespace ICreateAccount {
+  export type Input = Omit<Account, 'id'>
+  export type Output = {
+    accessToken: string
+    id: string
+  }
+}
+
+export interface ILoadAccountByToken {
+  execute (input: ILoadAccountByToken.Input): Promise<ILoadAccountByToken.Output>
+}
+
+export namespace ILoadAccountByToken {
+  export type Input = { accessToken: string }
+  export type Output = Account | null
+}
+
+export interface ILoadAccountByEmail {
+  execute (input: ILoadAccountByEmail.Input): Promise<ILoadAccountByEmail.Output>
+}
+
+export namespace ILoadAccountByEmail {
+  export type Input = { email: string }
+  export type Output = Account | null
+}
+
+
+export interface IUpdateAccount {
+  execute (input: IUpdateAccount.Input): Promise<IUpdateAccount.Output>
+}
+
+export namespace IUpdateAccount {
+  export type Input = {
+    accountId: string,
+    data: Partial<Omit<Account, 'id' | 'password'>>
+  }
+  export type Output = void
+}
---
+++ src/application/protocols/usecases/restaurant.usecase.protocol.ts
@@ -0,0 +1,32 @@
+import { Restaurant } from '@/domain/entities'
+
+export interface ICreateRestaurant {
+  execute (input: ICreateRestaurant.Input): Promise<ICreateRestaurant.Output>
+}
+
+export namespace ICreateRestaurant {
+  export type Input = Omit<Restaurant, 'id'> & { accountId: string }
+  export type Output = void
+}
+
+export interface ILoadRestaurantById {
+  execute (input: ILoadRestaurantById.Input): Promise<ILoadRestaurantById.Output>
+}
+
+export namespace ILoadRestaurantById {
+  export type Input = { id: string }
+  export type Output = Restaurant | null
+}
+
+export interface IUpdateRestaurant {
+  execute (input: IUpdateRestaurant.Input): Promise<IUpdateRestaurant.Output>
+}
+
+export namespace IUpdateRestaurant {
+  export type Data = Partial<Omit<Restaurant, 'id' | 'password'>>
+  export type Input = {
+    restaurantId: string,
+    data: Data
+  }
+  export type Output = void
+}
