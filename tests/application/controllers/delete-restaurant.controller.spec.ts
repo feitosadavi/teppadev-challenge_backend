7f2a98ac6653a9aaafd26e3a1279a261c671a8f6
feitosadavi <davifeitosa.dev@protonmail.com>
2022-08-02 19:44:44 -0300

    test(DeleteRestaurantController): ensure it calls loadRestaurantById with correct input

--- tests/application/controllers/delete-restaurant.controller.spec.ts
+++ tests/application/controllers/delete-restaurant.controller.spec.ts
@@ -1,32 +1,42 @@
 import { MockProxy, mock } from 'jest-mock-extended'
 
-import { IDeleteRestaurant } from '@/application/protocols'
+import { IDeleteRestaurant, ILoadRestaurantById } from '@/application/protocols'
 import { DeleteRestaurantController } from '@/application/controllers'
 import { noContent, serverError } from '@/application/helpers'
 
 const makeFakeRequest = (): DeleteRestaurantController.Request => ({
   restaurantId: 'any_id',
+  accountId: 'any_account_id',
 })
 
 describe('DeleteRestaurantController', () => {
   let sut: DeleteRestaurantController
 
+  let fakeLoadRestaurantById: MockProxy<ILoadRestaurantById>
   let fakeDeleteRestaurant: MockProxy<IDeleteRestaurant>
 
   let fakeRequest: DeleteRestaurantController.Request
 
   beforeAll(() => {
+    fakeLoadRestaurantById = mock()
     fakeDeleteRestaurant = mock()
 
     fakeRequest = makeFakeRequest()
   })
   beforeEach(() => {
-    sut = new DeleteRestaurantController(fakeDeleteRestaurant)
+    sut = new DeleteRestaurantController(fakeLoadRestaurantById, fakeDeleteRestaurant)
   })
 
   it('should call deleteRestaurant with correct input', async () => {
     await sut.handle(fakeRequest)
-    expect(fakeDeleteRestaurant.execute).toHaveBeenCalledWith(fakeRequest)
+    const { restaurantId: id } = fakeRequest
+    expect(fakeLoadRestaurantById.execute).toHaveBeenCalledWith({ id })
+  })
+
+  it('should call deleteRestaurant with correct input', async () => {
+    await sut.handle(fakeRequest)
+    const { restaurantId } = fakeRequest
+    expect(fakeDeleteRestaurant.execute).toHaveBeenCalledWith({ restaurantId })
   })
 
   it('should return 200 on success', async () => {
