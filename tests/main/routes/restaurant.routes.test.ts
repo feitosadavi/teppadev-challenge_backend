400945f3b954e21587fe6c93435aa0667a7e3699
feitosadavi <davifeitosa.dev@protonmail.com>
2022-08-02 20:54:23 -0300

    test(/restaurants/:restaurantId/delete): ensure it returns 200

--- tests/main/routes/restaurant.routes.test.ts
+++ tests/main/routes/restaurant.routes.test.ts
@@ -1,7 +1,6 @@
 import request from 'supertest'
 import { Express } from 'express'
 
-import { UpdateAccountController } from '@/application/controllers'
 import { setupApp } from '@/main/config'
 import { getFirestore } from 'firebase-admin/firestore'
 
@@ -43,14 +42,14 @@ describe('SignupRoutes', () => {
   //   db = getFirestore()
   // })
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    initializeApp({
+      credential: cert(FS_KEY as any),
+    }, `test-${Math.random()}`);
     app = setupApp()
   })
 
   afterEach(async () => {
-    initializeApp({
-      credential: cert(FS_KEY as any),
-    }, `test-${Math.random()}`);
     const db = getFirestore()
     for (const collectionName of ['accounts', 'restaurants']) {
       const collection = db.collection(collectionName)
@@ -76,6 +75,23 @@ describe('SignupRoutes', () => {
         .put(`/api/restaurants/${restaurantId}/update`)
         .set('x-access-token', accessToken)
         .send(params)
+
+      expect(status).toBe(204)
+      expect(body).toEqual({})
+    })
+  })
+
+  describe('DELETE /restaurants/:id/delete', () => {
+    it('should return 200 on success', async () => {
+      const accountId = await addAccount()
+      const accessToken = sign(accountId, 'secret')
+      updateAccessToken(accountId, accessToken)
+
+      const restaurantId = await addRestaurant(accountId)
+
+      const { status, body } = await request(app)
+        .delete(`/api/restaurants/${restaurantId}/delete`)
+        .set('x-access-token', accessToken)
 
       expect(status).toBe(204)
       expect(body).toEqual({})
