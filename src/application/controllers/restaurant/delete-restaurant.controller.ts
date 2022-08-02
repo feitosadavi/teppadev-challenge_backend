14587c7c46efa8952217030f0f3499a7e319bd75
feitosadavi <davifeitosa.dev@protonmail.com>
2022-08-02 19:44:37 -0300

    feat(DeleteRestaurantController): ensure it calls loadRestaurantById with correct input

--- src/application/controllers/restaurant/delete-restaurant.controller.ts
+++ src/application/controllers/restaurant/delete-restaurant.controller.ts
@@ -2,18 +2,21 @@ import {
   Controller,
   HttpResponse,
   IDeleteRestaurant,
+  ILoadRestaurantById,
 } from '@/application/protocols'
 import { noContent, serverError } from '@/application/helpers'
 
 export class DeleteRestaurantController implements Controller<DeleteRestaurantController.Request, DeleteRestaurantController.Reponse> {
   constructor(
+    private readonly loadRestaurantById: ILoadRestaurantById,
     private readonly deleteRestaurant: IDeleteRestaurant,
   ) { }
 
   async handle (req: DeleteRestaurantController.Request): Promise<HttpResponse<DeleteRestaurantController.Reponse>> {
     try {
-
-      await this.deleteRestaurant.execute(req)
+      const { accountId, restaurantId } = req
+      await this.loadRestaurantById.execute({ id: restaurantId })
+      await this.deleteRestaurant.execute({ restaurantId })
       return noContent()
 
     } catch (error) {
@@ -24,6 +27,7 @@ export class DeleteRestaurantController implements Controller<DeleteRestaurantCo
 
 export namespace DeleteRestaurantController {
   export type Request = {
+    accountId: string,
     restaurantId: string,
   }
   export type Reponse = {}
